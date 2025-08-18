04ac13175f017f4f71e8d050aca3e5c75c8c3a74
Niloy Mallik <niloymallik0001@gmail.com>
2025-08-18 15:53:45 +0530

    controller made

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,13 @@
 const registerUser = async(req,res)=>{
-    res.send("registered");
+    // get data
+    //vali date
+    // check if user already exixst 
+    // create a user in data base 
+    // ceate a verification tokaen 
+    // send token as email to user 
+    // send success status to user 
+
+
 };
 
 export {registerUser};
